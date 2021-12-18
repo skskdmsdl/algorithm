ed9eae1d78c1a922fa62eabdf41a21511db0b4d9
skskdmsdl <skskdmsdl@gmail.com>
2021-12-18 21:27:13 +0900

    문제 해설

--- baekjoon/2_그리디 알고리즘/11399_ATM.py
+++ baekjoon/2_그리디 알고리즘/11399_ATM.py
@@ -22,3 +22,14 @@ for i in range(n):
         num += s[j]
 print(num)
 
+# n = int(input()) # 사람 수 
+# arr = list(map(int,input().split())) # 인출 시간
+# arr.sort() # 정렬
+
+# result = 0
+
+# for i in range(1,n):
+#     arr[i] += arr[i-1] # 인출 시간 갱신
+
+# print(sum(arr))
+
