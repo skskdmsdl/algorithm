8293be7f00818c9fc71ec7261cc69a77331ba6fc
skskdmsdl <skskdmsdl@gmail.com>
2021-12-25 22:12:07 +0900

    문제 풀이2

--- baekjoon/2_그리디 알고리즘/2839_설탕 배달.py
+++ baekjoon/2_그리디 알고리즘/2839_설탕 배달.py
@@ -20,3 +20,17 @@ while sugar >= 0 :
     bag += 1  # 5의 배수가 될 때까지 설탕-3, 봉지+1
 else :
     print(-1)
+
+    
+# inp = int(input())
+# Box = 0
+# while True:
+#     if (inp % 5) == 0:
+#         Box = Box + (inp//5)
+#         print(Box)
+#         break
+#     inp = inp-3
+#     Box += 1
+#     if inp < 0:
+#         print("-1")
+#         break
