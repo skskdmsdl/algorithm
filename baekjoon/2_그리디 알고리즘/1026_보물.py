032856900db279838c42fd34f2b0a48dcf408036
skskdmsdl <skskdmsdl@gmail.com>
2021-12-27 21:54:09 +0900

    문제 풀이

--- baekjoon/2_그리디 알고리즘/1026_보물.py
+++ baekjoon/2_그리디 알고리즘/1026_보물.py
@@ -6,3 +6,16 @@
 # S의 최솟값을 출력하는 프로그램을 작성하시오.
 
 # 첫째 줄에 S의 최솟값을 출력한다.
+
+n = int(input())
+
+a_list = list(map(int, input().split()))
+b_list = list(map(int, input().split()))
+
+s = 0
+for i in range(n):
+    s += min(a_list) * max(b_list)
+    a_list.pop(a_list.index(min(a_list)))
+    b_list.pop(b_list.index(max(b_list)))
+
+print(s)
